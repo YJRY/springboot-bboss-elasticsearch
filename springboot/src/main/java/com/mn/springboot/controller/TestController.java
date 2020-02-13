56f4b5c004f4793fc2a22fd413f5fd207ca416f8
xuqi <xuqi@zed.com>
2020-02-13 15:46:28 +0800

    修改bboss版本，添加teacher数据

--- springboot/src/main/java/com/mn/springboot/controller/TestController.java
+++ springboot/src/main/java/com/mn/springboot/controller/TestController.java
@@ -1,13 +1,10 @@
 package com.mn.springboot.controller;
 
-import com.mn.springboot.pojo.Ftp;
-import com.mn.springboot.pojo.Teacher;
 import com.mn.springboot.utils.es.ESUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/test")
@@ -19,7 +16,6 @@ public class TestController {
 
     @RequestMapping("/test1")
     public Object test1() {
-        List<Ftp> t = esUtil.exec("ftp", new Ftp(), "test");
-        return t;
+        return null;
     }
 }
